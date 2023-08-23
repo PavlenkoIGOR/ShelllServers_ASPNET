a53fbce28c90ea226e16403e49eecbf7ced57ec1
PavlenkoIGOR <120670456+PavlenkoIGOR@users.noreply.github.com>
2023-08-23 13:05:29 +0300

    Update Index.js

--- ShelllServers_ASPNET/wwwroot/JS/Index.js
+++ ShelllServers_ASPNET/wwwroot/JS/Index.js
@@ -8,18 +8,58 @@ document.addEventListener('mousemove', event => {
 })
 
 
+let modalWindows = document.getElementsByClassName('modalWindow');
+
 /*показать окно для рекламы*/
 let advRef = document.getElementsByClassName('nav-link');
+
 for( var ref of advRef){
 	if(ref.innerHTML == "Реклама сервера"){
-		ref.addEventListener('click', Show);
+		ref.addEventListener('click', ShowAdwWindow);
 	}
-	if(ref.innerHTML == "Discord"){
-		ref.addEventListener('click', Show);
+	if(ref.innerHTML == "DISCORD"){
+		ref.addEventListener('click', ShowDiscord);
 	}
 	else{}
 }
-function Show(){
-	let advLink = document.getElementsByClassName('layers__advWindow')[0];
-	advLink.style.display = "flex";	
+
+/*делает все модальные невидимыми*/
+function HideModals(){
+	for(var modal of modalWindows){
+		modal.style.display = "none";
+	}
+}
+
+/*функция показа модального для дискорда*/
+function ShowDiscord(){
+	HideModals();
+	let discordWin = document.getElementById('layers__chatBotWindow');
+	discordWin.style.display = "block";
+}
+
+let position=0;
+/*функция показа модального для рекламы*/
+function ShowAdwWindow(){
+	HideModals();
+	let advWin = document.getElementById('layers__advWindow');
+	advWin.style.display = "flex";
+	Scroll();
+}
+
+
+
+function Scroll(){
+	let elem = document.getElementsByClassName('divFor')[0];
+	elem.addEventListener('wheel', function(e) {
+		let innerDivADV = document.getElementsByClassName('innerDivADV')[0];
+		if(e.wheelDelta >= 0)
+		{
+			position +=10;
+			innerDivADV.style.transform = `translateY(${position}px)`;
+		}
+		else{
+		position -=10;
+			innerDivADV.style.transform = `translateY(${position}px)`;
+		}
+	})
 }
